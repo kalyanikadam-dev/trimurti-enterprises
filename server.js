5e1f34c4b1c04c046533e64a312d1223e6cdc30b
Kalyani Kadam <kalyanikadam77013@gmail.com>
2026-05-17 17:29:47 +0530

    Hardcode frontend URL to bypass CORS

--- server.js
+++ server.js
@@ -32,10 +32,13 @@ app.use("/api", apiLimiter);
 // General Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Allow specific origins based on environment
+    origin: [
+      "https://trimurti-enterprises-frontend.vercel.app", 
+      "http://localhost:5173",
+      process.env.FRONTEND_URL
+    ].filter(Boolean), // Allow specific origins based on environment
     credentials: true,
   }),
-);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
